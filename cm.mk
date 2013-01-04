2228ecb803edab2fabb9bb2b8846e52647ce5b6c
AndroidMeda <hephappy@gmail.com>
2013-01-03 19:57:04 -0500

    p505: Use tiny config
    Change-Id: I1d550917cecbdb42060542a45b16dd5f41249594

--- cm.mk
+++ cm.mk
@@ -1,19 +1,14 @@
-# Boot Animation
-TARGET_SCREEN_HEIGHT := 480
-TARGET_SCREEN_WIDTH := 320
-
 # Include GSM stuff
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Inherit device configuration for p505.
 $(call inherit-product, device/lge/p505/p505.mk)
 
-# Inherit some common cyanogenmod stuff.
-$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
+TARGET_SCREEN_HEIGHT := 480
+TARGET_SCREEN_WIDTH := 320
+$(call inherit-product, vendor/cm/config/tiny.mk)
 
-# Include FM-Radio stuff
-#$(call inherit-product, vendor/cyanogen/products/bcm_fm_radio.mk)
-
+# Overrides
 PRODUCT_NAME := cm_p505
 PRODUCT_BRAND := lge
 PRODUCT_DEVICE := p505
@@ -21,12 +16,11 @@ PRODUCT_MODEL := LG-P505
 PRODUCT_MANUFACTURER := LGE
 PRODUCT_CHARACTERISTICS := phone
 
+# Release name and versioning
+PRODUCT_RELEASE_NAME := OptimusPhoenix
+PRODUCT_VERSION_DEVICE_SPECIFIC :=
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=lge_p505 \
     BUILD_FINGERPRINT="lge/thunderg/thunderg:2.3.3/GRI40/LG-P505-V20g.19C11F164C:user/release-keys" \
     PRIVATE_BUILD_DESC="thunderg-user 2.3.3 GRI40 LG-P505-V20g.19C11F164C release-keys"
-
-# Release name and versioning
-PRODUCT_RELEASE_NAME := OptimusPhoenix
-PRODUCT_VERSION_DEVICE_SPECIFIC :=
--include vendor/cyanogen/products/common_versions.mk
